4e2846e74df74a70c45997ed0b9e022adb681f93
kyoapps-kyo <kyo@kyoapps.com>
2022-11-27 17:29:28 +0900

    tabbar

--- MobileWeb/Mobile-project/src/components/TabBar.vue
+++ MobileWeb/Mobile-project/src/components/TabBar.vue
@@ -3,6 +3,44 @@ import { ref } from "vue";
 </script>
 
 <template>
-  <RouterLink to="/">首页</RouterLink>
+  <div>
+    <div class="tabbar">
+      <RouterLink to="/" class="tabbar-item">
+        <i class="iconfont icon-home"></i>
+        <span>首页</span>
+      </RouterLink>
+      <RouterLink to="/destination" class="tabbar-item">
+        <i class="iconfont icon-dest"></i>
+        <span>目的地</span>
+      </RouterLink>
+      <RouterLink to="/personal" class="tabbar-item">
+        <i class="iconfont icon-personal"></i>
+        <span>我的</span>
+      </RouterLink>
+    </div>
+  </div>
 </template>
-<style scoped></style>
+<style scoped>
+.tabbar {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  font-size: 12px;
+}
+.tabbar-item {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: #666;
+  font-size: 12px;
+}
+.router-link-exact-active {
+  color: #df2a2f;
+}
+.tabbar-item .iconfont {
+  margin-bottom: 1px;
+  font-size: 20px;
+}
+</style>
--- MobileWeb/Mobile-project/src/router/index.js
+++ MobileWeb/Mobile-project/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Index from "@/pages/index.vue";
+import Destination from "@/pages/destination.vue";
+import Personal from "@/pages/personal.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: "/",
       name: "Index",
       component: Index,
+    },
+    {
+      path: "/destination",
+      name: "Destination",
+      component: Destination,
+    },
+    {
+      path: "/personal",
+      name: "Personal",
+      component: Personal,
     },
   ],
 });
